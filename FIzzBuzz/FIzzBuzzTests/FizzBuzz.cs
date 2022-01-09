37bd3221cce11b4632ef2626e0f44b6e7fa9e757
Lily Fenton <lily@stevefenton.co.uk>
2022-01-09 13:02:46 +0000

    Move game logic into its own project.
    Create FizzBuzzFuzz variant.
    
    Make tests use data rows (part one).

--- FIzzBuzz/FIzzBuzzTests/FizzBuzz.cs
+++ FIzzBuzz/FIzzBuzzTests/FizzBuzz.cs
@@ -1,3 +1,4 @@
+using FizzBuzzGame;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FizzBuzzTests;
@@ -6,51 +7,50 @@ namespace FizzBuzzTests;
 public class FizzBuzz
 {
     [TestMethod]
-    public void CheckFizzBuzz_1_10()
+    public void NextSteps()
     {
-        var expected = "1,2,fizz,4,buzz,fizz,7,8,fizz,buzz";
+        Assert.Fail("Need to convert FizzBuzzFuzz test to be data driven, like FizzBuzz.");
+        Assert.Fail("Need to add cases for 15, 21, 35, 105 to both sets based on our matrix.");
+        Assert.Fail("Create the tests in SpecFlow.");
+    }
 
-        var actual = GetFizzBuzz(10);
+    [TestMethod]
+    [DataRow(1, "1")]
+    [DataRow(2, "2")]
+    [DataRow(3, "fizz")]
+    [DataRow(4, "4")]
+    [DataRow(5, "buzz")]
+    [DataRow(6, "fizz")]
+    [DataRow(7, "7")]
+    [DataRow(8, "8")]
+    [DataRow(9, "fizz")]
+    [DataRow(10, "buzz")]
+    public void FizzBuzzWith(int input, string expected)
+    {
+        var game = new Game();
+
+        var output = game.GetFizzBuzz(input);
+
+        string actual = GetItemAt(output, input);
 
         Assert.AreEqual(expected, actual);
     }
 
-
-    private string GetFizzBuzz(int max)
+    private string GetItemAt(string output, int position)
     {
-        string output = "";
-
-        for (int i = 0; i < max; i++)
-        {
-            int number = i + 1;
-
-            output = AddComma(output);
-
-            if (number % 3 == 0)
-            {
-                output += "fizz";
-                continue;
-            }
-
-            if (number % 5 == 0)
-            {
-                output += "buzz";
-                continue;
-            }
-
-            output += number.ToString();
-        }
-
-        return output;
+        return output.Split(',')[position - 1];
     }
 
-    private static string AddComma(string output)
+    [TestMethod]
+    public void CheckFizzBuzzFuzz_1_10()
     {
-        if (output.Length > 0)
-        {
-            output += ",";
-        }
+        var expected = "1,2,fizz,4,buzz,fizz,fuzz,8,fizz,buzz";
 
-        return output;
+        var game = new Game("FizzBuzzFuzz");
+
+        var actual = game.GetFizzBuzz(10);
+
+        Assert.AreEqual(expected, actual);
     }
 }
+
---
+++ FIzzBuzz/FizzBuzzGame/Game.cs
@@ -0,0 +1,59 @@
+﻿namespace FizzBuzzGame;
+
+public class Game
+{
+    private string _version;
+
+    public Game(string version = "FizzBuzz")
+    {
+        _version = version;
+    }
+
+    public string GetFizzBuzz(int max)
+    {
+        string output = "";
+
+        for (int i = 0; i < max; i++)
+        {
+            int number = i + 1;
+
+            output = AddComma(output);
+
+            if (number % 3 == 0)
+            {
+                output += "fizz";
+                continue;
+            }
+
+            if (number % 5 == 0)
+            {
+                output += "buzz";
+                continue;
+            }
+           
+            if (_version == "FizzBuzzFuzz")
+            {
+                if (number % 7 == 0)
+                {
+                    output += "fuzz";
+                    continue;
+                }
+            }
+
+            output += number.ToString();
+        }
+
+        return output;
+    }
+
+    private static string AddComma(string output)
+    {
+        if (output.Length > 0)
+        {
+            string outputWithComma = output + ",";
+            return outputWithComma;
+        }
+
+        return output;
+    }
+}
